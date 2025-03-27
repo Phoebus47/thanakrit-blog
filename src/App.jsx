1774188ab79c19813b65a0041e305e7d251bd110
Thanakrit <thanakrit.than.biz@gmail.com>
2025-03-27 19:49:23 +0700

    feat: replace local data with API for fetching posts, implement category-based filtering, and add routing for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import Footer from "./components/Footer";
-import ArticleSection from "./components/ArticleSection";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [backgroundLoaded, setBackgroundLoaded] = useState(false);
-
-  useEffect(() => {
-    const loadImage = async () => {
-      const img = new Image();
-      img.crossOrigin = 'anonymous';
-      img.src = "https://i.imgur.com/UASlokn.jpg"; // URL ของภาพพื้นหลัง
-      img.onload = () => setBackgroundLoaded(true);
-    };
-    loadImage();
-    
-    // ตั้งคุกกี้หลังจากโหลดภาพพื้นหลัง
-    document.cookie = "yourCookie=value; path=/; domain=example.com; Secure; SameSite=None";
-  }, []);
-
   return (
-    <div
-      className={`font-orbitron ${
-        backgroundLoaded
-          ? "bg-[url(https://i.imgur.com/UASlokn.jpg)]"
-          : "bg-slate-950" // สีพื้นหลังชั่วคราวระหว่างโหลด
-      } bg-fixed bg-cover md:bg-contain sm:bg-cover bg-center`}
-    >
-      <Navbar />
-      <HeroSection />
-      <ArticleSection />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          {/* <Route path="/post/:id" element={<PostPage />} /> */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
