e0be8300fedc679bb6ec8d83735440917af3a210
Thanakrit <thanakrit.than.biz@gmail.com>
2025-04-07 11:42:01 +0700

    fix: update font Orbitron import method to HTML link tag

---
+++ src/utils/preloadImage.js
@@ -0,0 +1,8 @@
+export function preloadImage(url) {
+    const link = document.createElement('link')
+    link.rel = 'preload'
+    link.as = 'image'
+    link.href = url
+    document.head.appendChild(link)
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["sans-serif"],
+        orbitron: ["Orbitron", "sans-serif"],
       },
     },
   },
