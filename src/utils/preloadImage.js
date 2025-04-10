83713b9088dd3e9edb0eddbc0d3126ab661c210b
Thanakrit <thanakrit.than.biz@gmail.com>
2025-04-10 16:28:11 +0700

    feat(server): support multiple CORS origins for development and production

--- src/utils/preloadImage.js
+++
@@ -1,8 +0,0 @@
-export function preloadImage(url) {
-    const link = document.createElement('link')
-    link.rel = 'preload'
-    link.as = 'image'
-    link.href = url
-    document.head.appendChild(link)
-  }
-  
