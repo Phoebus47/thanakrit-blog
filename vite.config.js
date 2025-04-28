1d76cff361088a396a2cf910bb6e1c2bfab22529
Thanakrit <thanakrit.than.biz@gmail.com>
2025-04-28 12:08:10 +0700

    feat: create ViewPostPage and NotFoundPage

--- vite.config.js
+++ vite.config.js
@@ -9,22 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
     },
-  },
-  define: {
-    __PROXY_BASE__: JSON.stringify(
-      process.env.NODE_ENV === "production"
-        ? "https://thanakrit-blog.vercel.app/proxy"
-        : "http://localhost:3001/proxy"
-    ),
   },
   build: {
     manifest: true,
     sourcemap: true,
-  },
-  server: {
-    proxy: {
-      '/proxy': 'http://localhost:3001',
-    },
   },
 });
